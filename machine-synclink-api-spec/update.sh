054214c3e0edaf3b38d934c8cee7fa69cd970c6f
daverolo <107847185+daverolo@users.noreply.github.com>
2022-10-10 00:45:36 +0200

    REFACTOR: bump version for synclinl-api-spec update script to 1.0.8

--- machine-synclink-api-spec/update.sh
+++ machine-synclink-api-spec/update.sh
@@ -31,7 +31,7 @@ cd ~
 #
 
 # Init basic vars
-ScriptVer="1.0.7"
+ScriptVer="1.0.8"
 HttpFileStorage="https://raw.githubusercontent.com/daverolo/helpers/main/machine-synclink-api-spec"
 
 # Init main vars
